993d27d234a76431f931ab888cbd7f97ae69fa8b
Admin <nebuloff@googlemail.com>
2024-08-17 11:21:35 +0300

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.5")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("com.google.dagger:hilt-android:2.48.1")
+    implementation("com.google.android.gms:play-services-auth:20.7.0")
     kapt("com.google.dagger:hilt-compiler:2.48.1")
     kapt("androidx.hilt:hilt-compiler:1.1.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
@@ -85,4 +86,12 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
---
+++ app/src/main/java/com/nebulov/cuppingformapp/feature_cup/presentation/sing_in/GoogleAuthUiClient.kt
@@ -0,0 +1,13 @@
+package com.nebulov.cuppingformapp.feature_cup.presentation.sing_in
+
+import android.content.Context
+import com.google.android.gms.auth.api.identity.SignInClient
+
+class GoogleAuthUiClient
+   ( private val context: Context,
+     private val oneTapClient: SignInClient
+           ){
+
+}
+
+
